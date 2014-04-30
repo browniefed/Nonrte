b4d2ef008e6577fa8b634a4dc4c4a939da806698
Jason Brown <iam@browniefed.com>
2014-04-30 07:10:19 -0700

    Laying out selection and range but not sure if it's necessary

---
+++ src/range/Range.js
@@ -0,0 +1,8 @@
+define([], function() {
+	var Range = function(from, to) {
+		this.from = from;
+		this.to = to;
+	};
+
+	return Range;
+});
---
+++ src/selection/Selection.js
@@ -0,0 +1,11 @@
+define([], function() {
+	var Selection = function(range) {
+		this.range = range;
+	};
+
+	Selection.prototype.highlight = function() {
+
+	};
+
+	return Selection;
+});
